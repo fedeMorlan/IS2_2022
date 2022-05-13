173058c86a0a9556cecc34c5d01eebe0b271a922
unknown <fedexmorlan@hotmail.com>
2022-05-12 22:04:04 -0300

    agregar base de datos e idioma espanol

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
 from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text='Last Name')
-    last_name = forms.CharField(max_length=100, help_text='Last Name')
-    email = forms.EmailField(max_length=150, help_text='Email')
+    first_name = forms.CharField(max_length=100, help_text='Nombre', label='Nombre')
+    last_name = forms.CharField(max_length=100, help_text='Apellido', label='Apellido')
+    email = forms.EmailField(max_length=150, help_text='Email', label='Email')
+    username = forms.CharField(max_length=150, help_text='Nombre de Usuario', label='Nombre de Usuario')
 
+    password1 = forms.CharField(
+        label="Contraseña",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label="Confirmación de contraseña",
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text="Ingresar la misma contraseña que antes, para verificación.",
+    )
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name',
--- accounts/models.py
+++ accounts/models.py
@@ -6,16 +6,77 @@ from django.dispatch import receiver
 
 # tal como se indica en https://dev.to/thepylot/create-advanced-user-sign-up-view-in-django-step-by-step-k9m
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Paciente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    dni = models.CharField(max_length=8,help_text='DNI')
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=150)
     bio = models.TextField()
+    # el password lo maneja otro api, por ahi esta bueno para que no figure el texto en la bd
 
     def __str__(self):
         return self.user.username
 
+
+class CentroDeVacunacion(models.Model):
+    nombre = models.CharField(max_length=100, primary_key=True)
+    direccion = models.CharField(max_length=100)
+
+
+class Vacuna(models.Model):
+    nombrevacuna = models.CharField(max_length=100, primary_key=True)
+    numero_de_lote = models.IntegerField
+    # YYYY-MM-DD
+    fecha_vencimiento = models.DateField
+
+
+class Aplicacion(models.Model):
+    # ya es primary key por ser AutoField
+    id_aplicacion = models.AutoField
+    nombrevacuna = models.CharField(max_length=100)
+    fecha_de_aplicacion = models.DateField
+    # no estoy seguro de como funciona el ForeignKey
+    id_paciente = models.ForeignKey
+    id_vacunador = models.ForeignKey
+
+
+class VacunasAnteriores(models.Model):
+    nombre_vacuna = models.CharField(max_length=100)
+
+
+class Pacientevacunas(models.Model):
+    id_pacientevacunas = models.AutoField
+    nombreusuario = models.ForeignKey
+    nombre_vacuna = models.ForeignKey
+
+
+class Turno(models.Model):
+    id_turno = models.AutoField
+    nombreusuario = models.ForeignKey
+    hora = models.TimeField
+    fecha = models.DateField
+
+
+class Vacunador(models.Model):
+    nombreusuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    dni = models.CharField(max_length=8)
+    email = models.EmailField(max_length=150)
+    # password
+
+
+class TrabajaEn(models.Model):
+    id_trabaja_en = models.AutoField
+    nombreusuario = models.ForeignKey
+    nombrecentro = models.ForeignKey
+
+
+class Dueno(models.Model):
+    nombreusuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    dni = models.CharField(max_length=8)
+    email = models.EmailField(max_length=150)
+    # password
+    
 
 @receiver(post_save, sender=User)
 def update_profile_signal(sender, instance, created, **kwargs):
