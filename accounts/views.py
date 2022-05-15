666ddb38e369f9de4fb63819305c0035105aa156
Segundo Ayllon <sasegundoayllon@gmail.com>
2022-05-15 18:24:02 -0300

    primer version vacunas anteriores

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import SignUpForm
+from .forms import SignUpForm, VacunasAnterioresForm
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from django.views.generic import RedirectView
@@ -41,5 +41,16 @@ def signup_view(request):
     # else:
     #    form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def vacunasAnteriores_view(request):
+    form = VacunasAnterioresForm(request.POST)
+    if form.is_valid():
+        vacun = form.save(commit = False)
+        vacun.user = request.user
+        vacun.save()
+
+    return render(request, 'vacunas_anteriores.html', {'form' : form})
+
+
 
 
